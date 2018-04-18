56c12f6433e31818883211446e30ee99977b1562
scg16@126.com <scg16@126.com>
2018-04-18 17:08:57 +0800

    添加服务注册中心Eureka

---
+++ laonong-center/src/main/java/com/laonog/admin/center/CenterBootstrap.java
@@ -0,0 +1,17 @@
+package com.laonog.admin.center;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * 启动一个服务注册中心提供给其他应用进行对话
+ * 服务注册中心启动 Eureka
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class CenterBootstrap {
+    public static void main(String[] args) {
+        SpringApplication.run(CenterBootstrap.class, args);
+    }
+}
