bc807ba1a3bd355d495ea1352da26e26c351f578
scg16@126.com <scg16@126.com>
2018-04-18 16:54:23 +0800

    spring cloud

---
+++ src/main/java/com/laonog/admin/LaonogAdminApplication.java
@@ -0,0 +1,12 @@
+package com.laonog.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaonogAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaonogAdminApplication.class, args);
+	}
+}
