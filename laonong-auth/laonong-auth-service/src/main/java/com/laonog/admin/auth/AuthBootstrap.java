abf347c1468428a5d84a601ae208321a037ef997
scg16@126.com <scg16@126.com>
2018-07-09 16:23:19 +0800

    添加auth模块

--- laonong-auth/laonong-auth-service/src/main/java/com/laonog/admin/auth/AuthBootstrap.java
+++ laonong-auth/laonong-auth-service/src/main/java/com/laonog/admin/auth/AuthBootstrap.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,6 +19,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @EnableEurekaClient
 @RestController
 @EnableAutoConfiguration
+@EnableFeignClients
 public class AuthBootstrap implements WebMvcConfigurer {
     public static void main(String[] args) {
         SpringApplication.run(AuthBootstrap.class, args);
